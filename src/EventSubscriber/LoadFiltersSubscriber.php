e8f2ddb8d29132514c0bd0d7bff6d24139450bfa
Tomas Votruba <tomas.vot@gmail.com>
2015-06-10 16:20:05 +0200

    use Symfony\EventDisptacher

---
+++ src/EventSubscriber/LoadFiltersSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ * Copyright (c) 2012 Tomas Votruba (http://tomasvotruba.cz)
+ */
+
+namespace Zenify\DoctrineFilters\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symnedi\EventDispatcher\Event\ApplicationRequestEvent;
+use Symnedi\EventDispatcher\NetteApplicationEvents;
+use Zenify\DoctrineFilters\Contract\FilterManagerInterface;
+
+
+class LoadFiltersSubscriber implements EventSubscriberInterface
+{
+
+	/**
+	 * @var FilterManagerInterface
+	 */
+	private $filterManager;
+
+
+	public function __construct(FilterManagerInterface $filterManager)
+	{
+		$this->filterManager = $filterManager;
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function getSubscribedEvents()
+	{
+		return [NetteApplicationEvents::ON_REQUEST => 'onRequest'];
+	}
+
+
+	public function onRequest(ApplicationRequestEvent $applicationRequestEvent)
+	{
+		$this->filterManager->enableFilters();
+	}
+
+}
